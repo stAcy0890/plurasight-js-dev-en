e925e2abdc883f5b1813d1f113e6926da25334a5
stAcy0890 <stacysarfo@hotmail.com>
2019-08-08 00:34:26 +0000

    Inline sourcemap added

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,9 +1,11 @@
 import path from 'path';
 
 export default {
+  debug: true,
   devtool: 'inline-source-map',
+  noInfo: false,
   entry: [
-    path.resolve(__dirname, 'src/index')
+path.resolve(__dirname, 'src/index')
   ],
   target: 'web',
   output: {
@@ -11,16 +13,11 @@ export default {
     publicPath: '/',
     filename: 'bundle.js'
   },
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-        debug: true,
-        noInfo: false,
-      })
-  ],
+  plugins: [],
   module: {
-    rules: [
-      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
-      {test: /\.css$/, loaders: ['style-loader','css-loader']}
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
     ]
   }
 }
